8f938960c8f2f5d656e62ded13fcb5ff88539092
juliangarciag05 <juliangarciag05@gmail.com>
2025-10-30 04:44:12 +0000

    añadi funcionalidad de delte

--- src/js/components/Form.jsx
+++ src/js/components/Form.jsx
@@ -4,10 +4,9 @@ const Form = () => {
 
     let [task, setTask] = useState("")
     let [list, setList] = useState([])
-
+    const API_URL = 'https://playground.4geeks.com/todo/'
 
     const createUser = () => {
-        const API_URL = 'https://playground.4geeks.com/todo/'
 
         fetch(API_URL + "users/Julian", {
             method: "POST",
@@ -22,7 +21,7 @@ const Form = () => {
     }
 
     const showTask = () => {
-        const API_URL = 'https://playground.4geeks.com/todo/'
+
 
         fetch(API_URL + "users/Julian")
 
@@ -45,36 +44,59 @@ const Form = () => {
 
     const createTask = async (text) => {
         try {
-            
+
             const response = await fetch(API_URL + "todos/Julian", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body : JSON.stringify({
+                body: JSON.stringify({
                     label: text,
                     is_done: false
                 })
             })
-              //verificar si la respuesta fue exitosa (codigo 200 - 299)
-            if (!response.ok){
-                throw new Error (`Error ${response.status}: No se pudo crear la tarea.`);
+            //verificar si la respuesta fue exitosa (codigo 200 - 299)
+            if (!response.ok) {
+                throw new Error(`Error ${response.status}: No se pudo crear la tarea.`);
             }
 
-            const data= await response.json()
+            const data = await response.json()
             console.log("tarea creada con exito:", data);
             showTask()
 
 
         } catch (error) {
-                console.error('hubo un problema al crear la tarea:', error)
+            console.error('hubo un problema al crear la tarea:', error)
         }
 
     }
 
+    const deleteTask = (id) => {
+        fetch(API_URL + "users/Julian", {
+            method: "DELETE",
+            header: {
+                "Content-Type": "application/json"
+            },
+
+        })
+            .then((response) => response.json())
+
+            .then((data => {
+                if (data) {
+                    setList(list.filter((task) => task.id !== id));
+                    console.log("tarea borrada con exito:", data);
+                }
+            }))
+            .catch(error => {
+                console.error('Hubo un problema:', error);
+            })
+
+    }
+
+
     const inputText = (event) => {
         if (event.key === "Enter") {
-            writeTask(task)
+            createTask(task)
             setTask("")
         }
     }
@@ -112,9 +134,9 @@ const Form = () => {
 
 
 
-                <input type="text" placeholder="What needs to be done?" className="form-control" onChange={createTask} value={task} onKeyDown={inputText} />
+                <input type="text" placeholder="What needs to be done?" className="form-control" onChange={(e) => setTask(e.target.value)} value={task} onKeyDown={inputText} />
                 <ul className="list-unstyled">
-                    {list.map((item) => (<li key={item.id}>{item.label}<span className="delete" onClick={() => removeTask(item.id)}> X</span></li>))}
+                    {list.map((item) => (<li key={item.id}>{item.label}<span className="delete" onClick={() => deleteTask(item.id)}> X</span></li>))}
                 </ul>
                 <div className="ms-0 container-fluid">
                     <p> {list.length} items left</p>
